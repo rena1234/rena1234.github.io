4ecfc14cbfb5611cb4c57abe2792c80c8bda0984
Renato Melo <renatoferreira249@gmail.com>
2020-08-22 16:32:04 -0300

    slider

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -9,6 +9,67 @@ styleElement.appendChild(document.createTextNode(style));
 const expanderClick = () => console.log('it works');
 const teste = () => expanderClick();
 
+const images = [
+  {
+    url: 'https://picsum.photos/id/495/720' 
+  },
+  {
+    url: 'https://picsum.photos/id/497/720' 
+  },
+  {
+    url: 'https://picsum.photos/id/496/720' 
+  },
+  {
+    url: 'https://picsum.photos/id/498/720' 
+  },
+  {
+    url: 'https://picsum.photos/id/499/720' 
+  },
+  {
+    url: 'https://picsum.photos/id/502/720' 
+  },
+  {
+    url: 'https://picsum.photos/id/503/720' 
+  },
+  {
+    url: 'https://picsum.photos/id/501/720' 
+  }
+]
+
+const getimagesHtml = (images) => {
+  const selectedIndex = 1;
+  let offset = ((100 * selectedIndex)) * -1;
+  let html = '';
+
+  for(const [index, item] of images.entries()){
+    if( index !== selectedIndex ) {
+      html += 
+      `
+        <div 
+          style="left: ${offset}vw;
+          background: url(${item.url});background-size: cover;
+          "
+          class="image unselected">
+        </div>
+      `
+      offset += 100;
+    } else {
+
+      html += 
+      `
+        <div 
+          style="left: ${offset}vw;
+          background: url(${item.url});background-size: cover;
+          "
+          class="image selected">
+        </div>
+      `
+      offset += 100;
+    }
+  }
+  return html;
+}
+
 const getHtml = (primaryColor) => {
     return `
       <div class="discover">
@@ -40,6 +101,7 @@ const getHtml = (primaryColor) => {
           </div>
         </div>
         <div class="image-container">
+          ${getimagesHtml(images)}
           <div class="image-container__text">This Page is under construction</div>
           <div class="image-container__expander">
             <expander-element>
@@ -73,32 +135,30 @@ const getHtml = (primaryColor) => {
     `;
 }
 
-const moveNext = (items) => {
-  const itemsSize = items.length;
-  const findNextIndex = (base, offset) => {
-    if(base + offset >= itemsSize){
-      return (base + offset) % itemsSize;
-    } else {
-      return base + offset;
-    }
-  }
-  const findPrevIndex= (base, offset) => {
-    if(base - offset < 0){
-      return itemsSize - Math.abs(base - offset);
-    } else {
-      return base - offset;
-    }
 
+const moveNext = (items, wrapper) => {
+  const firstItem = wrapper.querySelector('.image');
+  const firstItemClone = firstItem.cloneNode(true);
+  wrapper.removeChild(firstItem);
+  const itemVw = (items.length - 1) * 100;
+  firstItemClone.style['left'] = `${itemVw}vw`;
+  wrapper.appendChild(firstItemClone);
+  const newItems = wrapper.querySelectorAll('.image');
+  for(const [index, item] of newItems.entries()){
+    const stringPosition = item.style['left'];
+    const itemVw = parseInt(stringPosition.slice(0,stringPosition.length));
+    item.style['left'] = `${itemVw - 100}vw`;
   }
-  for(const [index, item] of items.entries()){
-    if(item.classList.contains('first')){
-      item.classList.remove('first');
-      item.classList.add('hidden-left');
-      items[findNextIndex(index,1)].classList.remove('second');
-      items[findNextIndex(index,1)].classList.add('first');
-      items[findNextIndex(index, 2)].classList.remove('hidden-right');
-      items[findNextIndex(index, 2)].classList.remove('hidden-left');
-      items[findNextIndex(index, 2)].classList.add('second');
+
+  for(const [index, item] of newItems.entries()){
+    if(item.classList.contains('selected')){
+      item.classList.remove('selected');
+      item.classList.add('unselected');
+      newItems[index + 1].classList.add('selected');
+      const stringPosition = newItems[index + 1].style['left'];
+      const itemVw = parseInt(stringPosition.slice(0,stringPosition.length - 2));
+      newItems[index + 1].style['left'] = `${itemVw}vw`;
+      newItems[index + 1].classList.remove('unselected');
       return true;
     }
   }
@@ -115,6 +175,9 @@ export default class Intro extends HTMLElement {
     this.shadowRoot.appendChild(styleElement);
     this.initExpander();
     this.initNumberButtons();
+    const wrapper = this.shadowRoot.querySelector('.image-container');
+    const images = this.shadowRoot.querySelectorAll('.image');
+    const interval = setInterval(() => moveNext(images, wrapper), 4000);
   } 
 
   initNumberButtons() {
