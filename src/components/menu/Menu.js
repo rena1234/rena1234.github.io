6b55a3fff92dadfa9cd4c046c4f59e1a4e1d9c9c
Renato Melo <renatoferreira249@gmail.com>
2020-08-25 09:11:28 -0300

    button icons on menu

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,53 +1,72 @@
 import style from 'Components/menu/Menu.scss';
 const template = document.createElement('template');
 
-const getHtml= (primaryColor, lightColor) => {
-  return `
-    <div class="menu__top">
-      <div class="menu__left">
+/*
         <div class="menu__left__bars">
           <slot name="menu-icon"/>
         </div>
+        */
+template.innerHTML = `
+    <div class="menu__top">
+      <div class="menu__left">
         <div
           data-section="footer-section"
-          class="menu__left__find hover-text link-button"
-          style="border-color: ${lightColor}"
+          class="menu__left__find hover-text link-button tooltip"
           >
-          <div
-            style="color: ${primaryColor}">
-            <slot name="person-icon"/>
-          </div>
+          <button>
+            <div>
+              <slot name="person-icon"/>
+            </div>
+          </button>
           contact me 
+          <span class="tooltip-text">contact me</span>
         </div>
         <div class="bar__name--left">
           <span>Renato</span><span class="primary-color">Melo</span>
         </div>
       </div>
       <div class="bar__name--center">
-        <span>Renato</span><span style="color: ${primaryColor}">Melo</span>
+        <span>Renato</span><span class="primary-color">Melo</span>
       </div>
       <div class="menu__right">
         <div 
           data-section="responsive-section"
-          class="menu__right__desktop-icon hover-text link-button"> 
-          <slot name="aspect-icon"/>
+          class="menu__right__desktop-icon hover-text link-button tooltip"> 
+          <span class="tooltip-text">responsive design</span>
+          <button>
+            <slot name="aspect-icon"/>
+          </button>
         </div>
         <div 
           data-section="experience-section"
-          class="hover-text link-button">
-          <slot name="shopping-icon"/>
+          class="hover-text link-button tooltip">
+          <span class="tooltip-text">my experience</span>
+          <button>
+            <slot name="shopping-icon"/>
+          </button>
         </div>
         <div 
           data-section="stack-section"
-          class="search-container link-button">
-          <div class="search-container__search hover-text">
+          class="hover-text link-button tooltip">
+          <span class="tooltip-text">my stack</span>
+          <button>
             <slot name="integration-icon"/>
-          </div>
+          </button>
         </div>
       </div>
     </div>
   `;
-}
+
+/*
+        <div 
+          data-section="stack-section"
+          class="search-container link-button tooltip">
+          <div class="search-container__search hover-text">
+            <slot name="integration-icon"/>
+          </div>
+          <span class="tooltip-text">my stack</span>
+        </div>
+        */
 
 const styleElement = document.createElement('style');
 styleElement.type = "text/css";
@@ -60,7 +79,6 @@ export default class Menu extends HTMLElement {
     this._primaryColor = 'black';
     this._lightColor= 'black';
     this._styleHoverElement = document.createElement('style');
-    template.innerHTML = getHtml(this._primaryColor); 
     this.attachShadow({mode: 'open'});
     this.shadowRoot.appendChild(template.content.cloneNode(true));
     this.shadowRoot.appendChild(styleElement);
@@ -68,19 +86,6 @@ export default class Menu extends HTMLElement {
   } 
   
   initButtons() {
-    /*
-    const scrollElements = [
-      document.querySelector('stack-section'),
-      document.querySelector('experience-section'),
-      document.querySelector('github-section'),
-    ];
-    const numberButtons = this.shadowRoot.querySelectorAll('.number');
-    numberButtons.forEach( btn => {
-      btn.addEventListener('click', () => {
-        scrollElements[btn.dataset.index].scrollIntoView({behavior: 'smooth'});
-      });
-    });
-    */
     const linkButtons = this.shadowRoot.querySelectorAll('.link-button');
     linkButtons.forEach( btn => {
       btn.addEventListener('click', () => {
